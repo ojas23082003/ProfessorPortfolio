94f35620584e271febc457b0c9d1bb55a57c8224
Ojas <ojas.modak23@gmail.com>
2023-07-09 20:53:21 +0530

    Fixed students page

--- portfolio/views.py
+++ portfolio/views.py
@@ -8,4 +8,22 @@ def publications(request):
     return render(request, 'publications.html',{'publications':True})
 
 def students(request):
-    return render(request, 'students.html')
+    return render(request, 'students.html',{'students':True})
+
+def biography(request):
+    return render(request, 'biography.html',{'biography':True})
+
+def projects(request):
+    return render(request, 'projects.html',{'projects':True})
+
+def consultancy(request):
+    return render(request, 'consultancy.html',{'consultancy':True})
+
+def collaborations(request):
+    return render(request, 'collaborations.html',{'collaborations':True})
+
+def fvisits(request):
+    return render(request, 'fvisits.html',{'fvisits':True})
+
+def others(request):
+    return render(request, 'others.html',{'others':True})
